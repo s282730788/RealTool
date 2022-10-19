182bca9789d6606b85bb6e014ac000d6c6e59a21
须尽欢 <56559302+s282730788@users.noreply.github.com>
2022-10-19 09:19:29 +0800

    Update config_ini.py

--- config_ini.py
+++ config_ini.py
@@ -19,5 +19,5 @@ def config_():
     config['font'] = {}
     config['font']['size'] = 26
     config['font']['color'] = '#505050'
-    config['real'] = {'douyu': '1', 'bili': '1', 'douyin': '1'}
+    config['real'] = {'douyu': '1', 'huya': '1', 'bili': '1', 'douyin': '1'}
     config.write()
