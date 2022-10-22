396b52560196d1fcdbf9f409ff24c0a5def0efde
须尽欢 <56559302+s282730788@users.noreply.github.com>
2022-10-22 15:32:50 +0800

    Update bili.py

--- real/bili.py
+++ real/bili.py
@@ -5,11 +5,12 @@
 # Software:PyCharm
 
 # 获取哔哩哔哩直播的真实流媒体地址，默认获取直播间提供的最高画质
-# qn=150高清
-# qn=250超清
-# qn=400蓝光
+# qn=1500高清
+# qn=2500超清
+# qn=4000蓝光
 # qn=10000原画
 import requests
+import re
 
 
 class BiliBili:
@@ -20,10 +21,10 @@ class BiliBili:
         Args:
             rid:
         """
-        # rid = rid
+        self.rid = rid
         self.header = {
             'User-Agent': 'Mozilla/5.0 (iPod; CPU iPhone OS 14_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, '
-            'like Gecko) CriOS/87.0.4280.163 Mobile/15E148 Safari/604.1',
+                          'like Gecko) CriOS/87.0.4280.163 Mobile/15E148 Safari/604.1',
         }
         # 先获取直播状态和真实房间号
         r_url = 'https://api.live.bilibili.com/room/v1/Room/room_init'
@@ -31,7 +32,7 @@ class BiliBili:
             'id': rid
         }
         with requests.Session() as self.s:
-            res = self.s.get(r_url, headers=self.header, params=param).json()
+            res = self.s.get(r_url, headers=self.header, params=param, timeout=2).json()
         if res['msg'] == '直播间不存在':
             raise Exception(f'bilibili {rid} {res["msg"]}')
         live_status = res['data']['live_status']
@@ -50,18 +51,9 @@ class BiliBili:
             'platform': 'web',
             'ptype': 8,
         }
-        res = self.s.get(url, headers=self.header, params=param).json()
+        res = self.s.get(url, headers=self.header, params=param, timeout=2).json()
         stream_info = res['data']['playurl_info']['playurl']['stream']
-        qn_max = 0
-
-        for data in stream_info:
-            accept_qn = data['format'][0]['codec'][0]['accept_qn']
-            for qn in accept_qn:
-                qn_max = qn if qn > qn_max else qn_max
-        if qn_max != current_qn:
-            param['qn'] = qn_max
-            res = self.s.get(url, headers=self.header, params=param).json()
-            stream_info = res['data']['playurl_info']['playurl']['stream']
+        accept_qn = stream_info[0]['format'][0]['codec'][0]['accept_qn']
 
         bili_dict = {}
         real_list = []
@@ -71,22 +63,36 @@ class BiliBili:
                 base_url = data['format'][-1]['codec'][0]['base_url']
                 url_info = data['format'][-1]['codec'][0]['url_info']
                 for i, info in enumerate(url_info):
-                    host = info['host']
-                    extra = info['extra']
-                    real_list.append({f'线路{i + 1}': f'{host}{base_url}{extra}'})
+                    for qn in accept_qn:
+                        url_ = base_url
+                        host = info['host']
+                        extra = info['extra']
+                        if qn < 10000:
+                            qn = qn * 10
+                            url_ = re.sub('bluray/index', f'{qn}/index', base_url)
+                        elif qn > 10000:
+                            continue
+
+                        extra = re.sub('qn=(\d+)', f'qn={qn}', extra)
+                        real_list.append({f'线路{i + 1}_{qn}': f'{host}{url_}{extra}'})
                 break
 
-
-        for count, real_ in enumerate(real_list):
+        for real_ in real_list:
             for url_ in real_:
                 try:
-                    code = requests.get(real_[url_], stream=True).status_code
+                    response = requests.get(real_[url_], stream=True, timeout=2)
+                    code = response.status_code
                     if code != 200:
-                        real_list.remove(real_list[count])
+                        real_list.remove(real_)
                 except:
-                    real_list.remove(real_list[count])
+                    real_list.remove(real_)
         if real_list:
+            real_list.append({'rid': self.rid})
             bili_dict['bili'] = real_list
         if bili_dict:
             return bili_dict
 
+# if __name__ == '__main__':
+#     rid = '6'
+#     bili = BiliBili(rid)
+#     print(bili.get_real_url())
